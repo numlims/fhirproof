98cf8ace53129f8edc3dca0f17f5e47808121f8e
numlims <max.haubold@med.uni-greifswald.de>
2024-07-10 13:28:51 +0200

    locationpath abschneiden raus

--- LocationCheck.py
+++ LocationCheck.py
@@ -18,22 +18,16 @@ class LocationCheck(FhirCheck):
         # restmenge zero
         if fh.restmenge(resource) == 0 and fh.lagerort(resource) != None:
             self.err(f"restmenge of sample {sampleid} is zero, it shouldn't have a lagerort but it's lagerort is {fh.lagerort(resource)}.")
-        # restmenge greater than zero
         if fh.restmenge(resource) > 0:
             locpath = fh.lagerort(resource)
             # print(f"lagerort of sample {sampleid}: {locpath}")
             if locpath == None:
                 self.err(f"no location path for sample {sampleid} in json, there should be one though.")
+
             query = "select * from centraxx_samplelocation where locationpath = ?"
             result = self.db.qfa(query, locpath)
             if len(result) == 0:
-                # check the shorter path, non-greedy matching
-                shortpath = re.sub(" -->[^>]*$", "", locpath)
-                result = self.db.qfa(query, shortpath)
-                if len(result) == 0:
-                    self.err(f"location {locpath} for sample {sampleid} is not in db.") 
-
-
+                self.err(f"location {locpath} for sample {sampleid} is not in db.") 
 
 
 
