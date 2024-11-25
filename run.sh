e4e0cf601b25931892da7b1768924ac6e4f6506d
tnustrings <max.haubold@med.uni-greifswald.de>
2024-11-25 14:19:47 +0100

    setuptools

--- run.sh
+++
@@ -1,3 +0,0 @@
-cat ../concat.gen.json | python3 fhirproof.py num_prod numddfhir --log-dir logs
-
-cat ../data/asavespecimen/2023-11-01_18-50-02-964-Specimen_P0.json | python3 fhirproof.py num_prod numddfhir --log-dir logs
