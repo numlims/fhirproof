aebf172a36e9a3b902531473d4ba7510aa39f757
numlims <max.haubold@med.uni-greifswald.de>
2025-10-20 10:58:29 +0200

    test

---
+++ test/test_db.py
@@ -0,0 +1,7 @@
+def test_db():
+    """
+    test_db tests fhirproof against db.
+    """
+    fp = fhirproof("num_prod", "numddfhir", "test/fp.log", configpath="config.yaml")
+    ok = fp.check("test/in", "latin-1")
+    assert ok = true
